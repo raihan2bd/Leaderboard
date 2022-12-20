31aa0fc4fc1d0e6c0610d35e9607679f66c9336f
raihan2bd <35267447+raihan2bd@users.noreply.github.com>
2022-12-20 18:23:17 +0600

    Fix linter errors from this project

--- src/index.js
+++ src/index.js
@@ -2,35 +2,35 @@ import './style.css';
 import { onAddscore, render } from './modules/manage-leaderboard.js';
 
 // Temporary data
-let data = [
+const data = [
   {
     id: 1,
-    name: "Raihan",
-    score: 100
+    name: 'Raihan',
+    score: 100,
   },
   {
     id: 2,
-    name: "Raihan",
-    score: 75
+    name: 'Raihan',
+    score: 75,
   },
   {
     id: 3,
-    name: "Raihan",
-    score: 50
+    name: 'Raihan',
+    score: 50,
   },
   {
     id: 4,
-    name: "Raihan",
-    score: 20
+    name: 'Raihan',
+    score: 20,
   },
   {
     id: 5,
-    name: "Raihan",
-    score: 10
-  }
+    name: 'Raihan',
+    score: 10,
+  },
 ];
 
-// ll the score list will append here. 
+// ll the score list will append here.
 const scoreContainer = document.querySelector('.score-list-group');
 
 // on submit add score
@@ -39,13 +39,13 @@ addBookForm.addEventListener('submit', (e) => {
   e.preventDefault();
   const name = document.getElementById('name').value;
   const score = document.getElementById('score').value;
-  onAddscore();
+  onAddscore({ name, score });
 
   // next level code will start form here
-})
+});
 
 // onload fetch the score.
 window.onload = () => {
   // I will face the scrore form here.
   render(data, scoreContainer);
-}
+};
--- src/modules/manage-leaderboard.js
+++ src/modules/manage-leaderboard.js
@@ -1,20 +1,15 @@
-export const onAddscore = (data) => {
-  return data;
-}
-
+export const onAddscore = (data) => data;
 
 export const render = (data, domContainer) => {
   let domUI = '';
-  if(data.length>0) {
-
-    data.forEach(item => {
+  if (data.length > 0) {
+    data.forEach((item) => {
       domUI += `<li class='score-item' id='${item.id}'>
       ${item.name}: <span class="badge-score">${item.score}</span>
-      </li>`
+      </li>`;
     });
-  
   } else {
-    domUI = `<p class='no-item'>There is not score to display</p>`
+    domUI = '<p class=\'no-item\'>There is not score to display</p>';
   }
   domContainer.innerHTML = domUI;
-}
+};
