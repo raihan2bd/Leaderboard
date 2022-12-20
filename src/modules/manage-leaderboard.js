22ea58b551d7e9f6cae0cf15239e3d760a060fe0
raihan2bd <35267447+raihan2bd@users.noreply.github.com>
2022-12-20 18:04:24 +0600

    Add Javascript functionality to display the score list

---
+++ src/modules/manage-leaderboard.js
@@ -0,0 +1,20 @@
+export const onAddscore = (data) => {
+  return data;
+}
+
+
+export const render = (data, domContainer) => {
+  let domUI = '';
+  if(data.length>0) {
+
+    data.forEach(item => {
+      domUI += `<li class='score-item' id='${item.id}'>
+      ${item.name}: <span class="badge-score">${item.score}</span>
+      </li>`
+    });
+  
+  } else {
+    domUI = `<p class='no-item'>There is not score to display</p>`
+  }
+  domContainer.innerHTML = domUI;
+}
