d0317333790f1ceb909dc0ae3fadcadfe3e9a85f
raihan2bd <35267447+raihan2bd@users.noreply.github.com>
2022-12-21 16:48:45 +0600

    Add and fetch all scores from the api

--- src/modules/manage-leaderboard.js
+++ src/modules/manage-leaderboard.js
@@ -1,15 +1,37 @@
-export const onAddscore = (data) => data;
-
-export const render = (data, domContainer) => {
+// Render the game scores
+export const render = (data) => {
+  const scoreContainer = document.querySelector('.score-list-group');
   let domUI = '';
   if (data.length > 0) {
     data.forEach((item) => {
-      domUI += `<li class='score-item' id='${item.id}'>
-      ${item.name}: <span class="badge-score">${item.score}</span>
+      domUI += `<li class='score-item'>
+      ${item.user}: <span class="badge-score">${item.score}</span>
       </li>`;
     });
   } else {
-    domUI = '<p class=\'no-item\'>There is not score to display</p>';
+    domUI = '<p class=\'no-item\'>There is no score to display! Please add a new one</p>';
   }
-  domContainer.innerHTML = domUI;
+  // append scores in scoreContainer
+  scoreContainer.innerHTML = domUI;
+};
+
+// Add score
+export const onAddscore = async (url, data) => {
+  const res = await fetch(url, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(data),
+  });
+
+  const result = await res.json();
+  return result;
+};
+
+// fetch all game scores
+export const onFetchData = async (url) => {
+  const res = await fetch(url);
+  const data = await res.json();
+  render(data.result);
 };
